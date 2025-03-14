ce60302ed93865e460fc653b231c73a4c374d402
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-14 03:50:08 +0100

    Rework goldens

--- test/setup/goldens.ts
+++ test/setup/goldens.ts
@@ -4,24 +4,36 @@
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-import { readFile, writeFile } from 'fs/promises';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
 import { expect } from 'vitest';
 
 /// If this is set to true, the golden files will be updated.
-export const updateGoldens = () => {
+export const shouldUpdateGoldens = () => {
   return process.env['UPDATE_GOLDENS'] === 'true';
 };
 
-export const checkGoldens = async (goldenFile: string, expected: string) => {
-  // Write golden file
-  if (updateGoldens()) {
-    await writeFile(goldenFile, expected);
-  }
+/**
+ * Updates the golden file with the given content.
+ * @param filexName - The golden file to compare with
+ * @param expected - The expected golden content
+ */
+export const expectGolden = (fileName: string) => {
+  return {
+    toBe: (expected: string) => {
+      const filePath = join(__dirname, '..', 'goldens', fileName);
 
-  // Compare log messages with golden file
-  const golden = await readFile(goldenFile, 'utf8');
-  const message =
-    `Golden "${goldenFile}" does not match expected content. ` +
-    'Consider to rebuild goldens using "npm run updateGoldens".';
-  expect(golden, message).toBe(expected);
+      // Write golden file
+      if (shouldUpdateGoldens()) {
+        writeFileSync(filePath, expected);
+      }
+
+      // Compare log messages with golden file
+      const golden = readFileSync(filePath, 'utf8');
+      const message =
+        `Golden "${fileName}" does not match expected content. ` +
+        'Consider to rebuild goldens using "npm run updateGoldens".';
+      expect(golden, message).toBe(expected);
+    },
+  };
 };
